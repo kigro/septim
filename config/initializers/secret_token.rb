37386105aa28308c5ba776d25dd39c34ab1b75b8
Kim-Kjetil Grosland <kim.kjetil.grosland@gmail.com>
2013-08-07 14:07:40 +0200

    lagt til controller

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
+require 'securerandom'
+def secure_token
+	token_file = Rails.root.join('.secret')
+	if File.exist?(token_file)
+		# Use the existing token.
+		File.read(token_file).chomp
+	else
+	    # Generate a new token and store it in token_file.
+	    token = SecureRandom.hex(64)
+	    File.write(token_file, token)
+	    token
+	end 
+end
+
+
 SeptimApp::Application.config.secret_key_base = 'f3a578d08a343df6fbb81806febc9afff1e77139b92510287ebea5a39322ff8c4d6f338701e93bf2141176cc74ad42a412b6e3ddefa05076726becd519411a71'
